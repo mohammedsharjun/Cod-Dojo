a0f49b439d34690944b91688439cffbf938d3a65
mohammedsharjun <dhanishsharjun9@gmail.com>
2024-09-03 17:19:27 +0530

    nav

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     gsap.set(".img", { y: 500 });
     gsap.set(".loader-img", { x: 500 });
-    gsap.set(".nav-item, section", { y: 25, opacity: 0 });
+    gsap.set(".nav-item", { y: 25, opacity: 0 });
     gsap.set("h1, .item, footer", { y: 200 });
 
     const t1 = gsap.timeline({ delay: 0.5 });
@@ -42,5 +42,68 @@ document.addEventListener("DOMContentLoaded", function () {
     }, "-=0.5");
 });
 
+const card3 = document.querySelector("#card-3");
+const toggleButton = document.querySelector("#togglebutton"); // Corrected selector
+const cards = document.querySelectorAll(".card");
+const toggleDiv = document.querySelector(".toggle");
+const siteContent = document.querySelector(".site-content");
+let isCardOpen = false;
 
+card3.addEventListener("click", () => {
+  if (!isCardOpen) {
+    gsap.to(cards, {
+      top: "0px",
+      duration: 1,
+      ease: "power4.inOut",
+    });
+    gsap.to(toggleDiv, {
+      delay: 0.4,
+      display: "flex",
+      opacity: 1,
+      duration: 0.4,
+      ease: "power4.inOut",
+    });
+    gsap.to(siteContent, {
+      filter: "blur(20px)", // Corrected closing parenthesis
+      duration: 0.75,
+      ease: "power4.inOut",
+    });
+    isCardOpen = true;
+  }
+});
 
+toggleButton.addEventListener("click", () => {
+  if (isCardOpen) {
+    gsap.to("#card-1", {
+      top: "0px",
+      duration: 1,
+      ease: "power4.inOut",
+    });
+    gsap.to("#card-2", { 
+      top: "-100px",
+      duration: 1,
+      ease: "power4.inOut",
+    });
+    gsap.to("#card-3", { 
+      top: "-200px",
+      duration: 1,
+      ease: "power4.inOut",
+    });
+
+    gsap.to(toggleDiv, {
+      delay: 0,
+      display: "none",
+      opacity: 0,
+      duration: 0.4,
+      ease: "power4.inOut",
+    });
+
+    gsap.to(siteContent, {
+      filter: "blur(0px)",
+      duration: 1,
+      ease: "power4.inOut",
+    });
+
+    isCardOpen = false;
+  }
+});
